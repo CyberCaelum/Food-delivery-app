328bdef77cb310262324c6bcd75e6e12d0ed8244
CyberCaelum <cybercaelum>
2025-06-25 20:34:16 +0800

    添加了前端文件，但是无法访问

---
+++ sky-common/src/main/java/com/sky/exception/CategorySortEqual.java
@@ -0,0 +1,13 @@
+package com.sky.exception;
+
+/**
+ * @ClassName : CategorySortEqual
+ * @Description : 分类排序相等错误
+ * @Author :  CyberCaelum
+ * @Date: 2025-06-25 11:14
+ */
+public class CategorySortEqual extends BaseException{
+    public CategorySortEqual(String msg) {
+        super(msg);
+    }
+}
